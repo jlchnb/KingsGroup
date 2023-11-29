004d3a3c115e265b844795f908651fae1923d5bb
jlchn <jncohenb@gmail.com>
2023-11-29 18:08:45 -0300

    ayayai

--- src/app/emergency-assist/emergency-assist.page.ts
+++ src/app/emergency-assist/emergency-assist.page.ts
@@ -7,26 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmergencyAssistPage implements OnInit {
 
-  isModalOpen = false;
-  isImageExpanded = false;
-
   constructor() { }
 
   ngOnInit() {
   }
 
-  animateAndOpenModal() {
-    // Cambia la clase de la imagen para iniciar la animación de escala
-    this.isImageExpanded = true;
-
-    // Después de un breve retraso, abre el modal
-    setTimeout(() => {
-      this.isModalOpen = true;
-    }, 300); // 300 milisegundos para que coincida con la duración de la animación CSS
-  }
+  isModalOpen = false;
 
   setOpen(isOpen: boolean) {
     this.isModalOpen = isOpen;
-    this.isImageExpanded = false; // Restablece la propiedad isImageExpanded al cerrar el modal
   }
 }
